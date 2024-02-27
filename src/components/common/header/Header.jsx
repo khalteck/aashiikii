45463e067e27c5729caf8eefbce370289e1915ae
khalteck <khalidoyeneye@gmail.com>
2024-02-27 10:33:19 +0100

    feat: search fix

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,7 +1,7 @@
 import TopStrip from "./TopStrip";
 import SearchCont from "./SearchCont";
 import Nav from "./Nav";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import nav from "../../../data/nav.json";
 import Dropdown from "./Dropdown";
 import { useAppContext } from "../../../contexts/AppContext";
@@ -12,12 +12,21 @@ import { IoClose } from "react-icons/io5";
 const Header = () => {
   const { openSearch, setOpenSearch } = useAppContext();
 
-  function toggleSearch() {
-    setOpenSearch((prev) => !prev);
-  }
   const [openMenu, setOpenMenu] = useState(false);
   function handleMenu() {
     setOpenMenu((prev) => !prev);
+  }
+
+  const [showList, setShowList] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setShowList(true);
+    }, 600);
+  }, [openSearch]);
+
+  function toggleSearch() {
+    setOpenSearch((prev) => !prev);
+    setShowList((prev) => !prev);
   }
 
   return (
@@ -41,42 +50,44 @@ const Header = () => {
       {openSearch && (
         <div
           onClick={toggleSearch}
-          className="w-full h-screen bg-neutral-800/80 fixed top-0 left-0 py-[100px] px-5 z-[100] blurry"
+          className="w-full h-screen bg-neutral-800/80 fixed top-0 left-0 py-[100px] px-5 z-[100] blurry bgslide"
         >
           <div className="w-[50px] h-[50px] p-3 flex items-center justify-center bg-neutral-50 cursor-pointer absolute top-5 right-5 md:right-[50%] md:translate-x-[50%]">
             <IoClose color="black" size="25px" />
           </div>
-          <div
-            onClick={(e) => e.stopPropagation()}
-            className="bg-neutral-50 w-full sm:w-[550px] h-[80%] sm:h-[700px] min-h-[400px] mx-auto overflow-y-auto relative"
-          >
-            <div className="">
-              <div className="w-full min-w-[200px] flex border border-neutral-950/20">
-                <div className="w-[50px] md:h-[55px] p-3 flex items-center justify-center bg-[#F1E4D8] cursor-pointer">
-                  <AiOutlineSearch color="black" size="25px" />
+          {showList && (
+            <div
+              onClick={(e) => e.stopPropagation()}
+              className="bg-neutral-50 w-full sm:w-[550px] h-[80%] sm:h-[700px] min-h-[400px] mx-auto overflow-y-auto relative dropslide"
+            >
+              <div className="">
+                <div className="w-full min-w-[200px] flex border border-neutral-950/20">
+                  <div className="w-[50px] md:h-[55px] p-3 flex items-center justify-center bg-[#F1E4D8] cursor-pointer">
+                    <AiOutlineSearch color="black" size="25px" />
+                  </div>
+                  <input
+                    type="text"
+                    className="bg-white w-full text-black p-3 md:p-4 outline-none rounded-none text-[.95rem] placeholder:text-black/50"
+                    placeholder="SEARCH.."
+                  />
                 </div>
-                <input
-                  type="text"
-                  className="bg-white w-full text-black p-3 md:p-4 outline-none rounded-none text-[.95rem] placeholder:text-black/50"
-                  placeholder="SEARCH.."
-                />
-              </div>
 
-              <div className="mt-3 flex flex-col gap-3 p-4">
-                <p>
-                  <span className="font-bold">6</span> Search Result(s)..
-                </p>
-              </div>
+                <div className="mt-3 flex flex-col gap-3 p-4">
+                  <p>
+                    <span className="font-bold">6</span> Search Result(s)..
+                  </p>
+                </div>
 
-              <div className="w-full flex flex-col border-t border-neutral-950/20">
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
-                <SearchCard />
+                <div className="w-full flex flex-col border-t border-neutral-950/20">
+                  <SearchCard />
+                  <SearchCard />
+                  <SearchCard />
+                  <SearchCard />
+                  <SearchCard />
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       )}
     </div>
--- src/components/common/header/SearchCont.jsx
+++ src/components/common/header/SearchCont.jsx
@@ -8,10 +8,7 @@ const SearchCont = ({ handleMenu, toggleSearch }) => {
   const navigate = useNavigate();
 
   return (
-    <div
-      onClick={toggleSearch}
-      className="w-full bg-neutral-950 text-white text-[.9rem] px-5 md:px-[100px] flex gap-4 md:gap-12 items-center pt-4 pb-4 md:pb-5"
-    >
+    <div className="w-full bg-neutral-950 text-white text-[.9rem] px-5 md:px-[100px] flex gap-4 md:gap-12 items-center pt-4 pb-4 md:pb-5">
       <div>
         <img
           alt="logo"
@@ -22,7 +19,11 @@ const SearchCont = ({ handleMenu, toggleSearch }) => {
           <HiMenuAlt2 size="25px" color="white" />
         </div>
       </div>
-      <div className="w-full min-w-[200px] flex mx-2 md:mx-0">
+      <div className="w-full min-w-[200px] flex mx-2 md:mx-0 relative">
+        <div
+          onClick={toggleSearch}
+          className="w-full h-full absolute top-0 left-0 cursor-pointer hover:bg-white/30"
+        ></div>
         <div className="w-[50px] md:h-[55px] p-2 md:p-3 flex items-center justify-center bg-[#F1E4D8]">
           <AiOutlineSearch color="black" size="25px" />
         </div>
